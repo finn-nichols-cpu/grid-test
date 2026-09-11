50f53fc529dfbd7aa8c8b653a22182b86c2b8bee
innfinnion <finn-nichols@rdschool.org>
2026-09-11 15:05:21 -0700

    Changed what happened when clicked so it is set to the X icon and doesn't get changed back when released.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const gridButtons = document.querySelectorAll(".grid-object");
 gridButtons.forEach(button => {
-    button.addEventListener()
+    button.addEventListener("click", () => {(button as HTMLElement).className = "grid-object-clicked"});
 });
