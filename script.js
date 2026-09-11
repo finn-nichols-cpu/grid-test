17a933e81989ea27a198368c1ea3801f7b4b2bc0
innfinnion <finn-nichols@rdschool.org>
2026-09-11 15:12:00 -0700

    Test

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+/** @type {NodeListOf<HTMLElement>} */
 const gridButtons = document.querySelectorAll(".grid-object");
+
 gridButtons.forEach(button => {
-    button.addEventListener("click", () => {(button as HTMLElement).className = "grid-object-clicked"});
+    button.addEventListener("click", () => {
+        button.className = "grid-object-clicked";
+    });
 });
